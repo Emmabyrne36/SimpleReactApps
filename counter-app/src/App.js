722251eefb0cdec0399957304f13bd1254f07333
Emmabyrne36 <emmabyrne36@gmail.com>
2019-04-26 11:03:24 +0100

    Committing counter app

---
+++ counter-app/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import NavBar from "./Components/navbar";
+import "./App.css";
+import Counters from "./Components/counters";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    // Remove from the array
+    this.setState({
+      counters: this.state.counters.filter(item => item.id !== counterId)
+    });
+  };
+
+  handleReset = () => {
+    console.log("In the reset");
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]; // clone the array using the spread operator
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
+
+// Lifting state up 2:00:41
